ec161b2b8c55e6afdf85fd11b270a0990401e3c7
Gabriel Falcão <gabriel@nacaolivre.org>
2021-12-12 08:42:54 +0100

    fix load test

--- aestest.sh
+++ aestest.sh
@@ -2,7 +2,17 @@
 
 password='I X@X@ Nickelback <3'
 plaintext="Hello World"
-bin="./target/release/aes256"
+plaintext_filename="plain.txt"
+
+if [ ! -e "${plaintext_filename}" ]; then
+    echo -n "Hello World" > $plaintext_filename
+fi
+
+if [ -x ./target/release/aes-256-cbc ]; then
+    bin="./target/release/aes-256-cbc";
+else
+    bin="./target/debug/aes-256-cbc";
+fi
 #auth=--key-filename "${key_filename}"
 #auth=--password "${password}"
 
@@ -16,10 +26,9 @@ done
 for i in $(seq 10); do
     export key_filename="key${i}.yaml";
     export cyphertext_filename="cyphertext${i}.aes";
-    ${bin} encrypt --password "${password}" --cyphertext-filename "${cyphertext_filename}" --input-filename Cargo.toml;
-    # ${bin} encrypt --password "${password}" --cyphertext-filename "${cyphertext_filename}" --string "${plaintext}";
-
-    ${bin} decrypt --key-filename "${key_filename}" --cyphertext-filename "${cyphertext_filename}";
+    ${bin} encrypt --key-filename "${key_filename}" --input-filename "${plaintext_filename}" --output-filename "${cyphertext_filename}"
+    ${bin} decrypt --key-filename "${key_filename}" --input-filename "${cyphertext_filename}" --output-filename "${plaintext_filename}"
+    test "$(cat $plaintext_filename)" == "${plaintext}"
 done
 
 
@@ -29,7 +38,9 @@ for i in $(seq 10); do
         export key_filename="key${j}.yaml";
         export cyphertext_filename="cyphertext${i}.aes";
         echo "${key_filename}: ${cyphertext_filename}"
-        ${bin} decrypt --key-filename "${key_filename}" --cyphertext-filename "${cyphertext_filename}";
-        ${bin} decrypt --password "${password}" --cyphertext-filename "${cyphertext_filename}";
+        ${bin} decrypt --key-filename "${key_filename}" --input-filename "${cyphertext_filename}" --output-filename "${plaintext_filename}"
+        ${bin} decrypt --password "${password}" --input-filename "${cyphertext_filename}" --output-filename "${plaintext_filename}"
+        test "$(cat $plaintext_filename)" == "${plaintext}"
+
     done
 done
